43a793df400cd2a7b58b5d0073a0d6c80e81247c
Kangho Lee <cannalee90@gmail.com>
2018-05-12 22:02:36 +0900

    chore 기존에 설정되어 있던 eslint 및 eslintignore 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,78 @@
-// https://eslint.org/docs/user-guide/configuring
+const isDev = process.env.NODE_ENV !== "production",
+    e = isDev ? "warn" : "error";
 
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  env: {
-    browser: true,
-  },
-  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-  extends: ['plugin:vue/essential', 'airbnb-base'],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // check if imports actually resolve
-  settings: {
-    'import/resolver': {
-      webpack: {
-        config: 'build/webpack.base.conf.js'
-      }
+    "root": true,
+    "env": {
+        "browser": true,
+        "es6": true,
+        "webextensions": true,
+        "node": true,
+    },
+    "extends": "eslint:recommended",
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": ["html"],
+    "globals": {
+        "console": isDev
+    },
+    "rules": {
+        // Possible Errors
+        "no-console": e,
+        "no-debugger": e,
+
+        // Best Practices
+        "curly": e,
+        "dot-notation": e,
+        "eqeqeq": e,
+        "no-else-return": e,
+        "no-eq-null": e,
+        "no-extra-bind": e,
+        "no-lone-blocks": e,
+        "no-useless-call": e,
+        "no-useless-return": e,
+        "no-void": e,
+        "no-with": e,
+        "radix": e,
+        "yoda": e,
+
+        // Variables
+        "no-unused-vars": ["warn", {
+            "vars": "all",
+            "args": "all",
+            "argsIgnorePattern": "(^__|^h$)"
+        }],
+
+        // Stylistic Issues
+        "comma-dangle": e,
+        "indent": ["warn", 4],
+        "max-len": ["warn", {
+            "code": 80,
+            "ignoreComments": true,
+            "ignoreStrings": true,
+            "ignoreTemplateLiterals": true,
+            "ignoreRegExpLiterals": true
+        }],
+        "one-var": ["warn", { "var": "always" }],
+        "quote-props": ["warn", "as-needed"],
+        "quotes": ["warn", "double", { "avoidEscape": true }],
+        "semi": ["warn", "always"],
+
+        // ECMAScript 6
+        "arrow-body-style": e,
+        "arrow-parens": [e, "as-needed"],
+        "no-useless-computed-key": e,
+        "no-useless-rename": e,
+        "no-var": e,
+        "object-shorthand": [e, "always"],
+        "prefer-arrow-callback": e,
+        "prefer-const": [e, { "destructuring": "any" }],
+        "prefer-destructuring": e,
+        "prefer-template": e
     }
-  },
-  // add your custom rules here
-  rules: {
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      js: 'never',
-      vue: 'never'
-    }],
-    // disallow reassignment of function parameters
-    // disallow parameter object manipulation except for specific exclusions
-    'no-param-reassign': ['error', {
-      props: true,
-      ignorePropertyModificationsFor: [
-        'state', // for vuex state
-        'acc', // for reduce accumulators
-        'e' // for e.returnvalue
-      ]
-    }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      optionalDependencies: ['test/unit/index.js']
-    }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
-}
+};
